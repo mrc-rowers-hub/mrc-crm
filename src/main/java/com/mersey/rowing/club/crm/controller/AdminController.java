f89dca578e559dd73108cd8b98c4d0ccbe95a002
samuellsrobertson <samlsrobertson@gmail.com>
2024-12-20 11:27:41 +0000

    - Added the code for generating the UUID's in AdminController
    - Added @Service to CreationCodesService

--- src/main/java/com/mersey/rowing/club/crm/controller/AdminController.java
+++ src/main/java/com/mersey/rowing/club/crm/controller/AdminController.java
@@ -1,15 +1,25 @@
 package com.mersey.rowing.club.crm.controller;
 
+import com.mersey.rowing.club.crm.controller.service.CreationCodesService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @Slf4j
 public class AdminController {
+
+    @Autowired
+    private CreationCodesService creationCodesService;
+
     @GetMapping("/request_new_users")
-    public void requestNewUsers(@RequestParam(defaultValue = "1") int amount) {
-        log.info("Amount: " + amount);
+    public Map<String, List<String>> requestNewUsers(@RequestParam(defaultValue = "1") int amount) {
+        List<String> codes = creationCodesService.createNewUUID(amount);
+        return Map.of("creation_codes", codes);
     }
 }
--- src/main/java/com/mersey/rowing/club/crm/controller/service/CreationCodesService.java
+++ src/main/java/com/mersey/rowing/club/crm/controller/service/CreationCodesService.java
@@ -3,12 +3,14 @@ package com.mersey.rowing.club.crm.controller.service;
 import com.mersey.rowing.club.crm.model.repository.UserCreationCode;
 import com.mersey.rowing.club.crm.model.repository.UserCreationCodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class CreationCodes {
+@Service
+public class CreationCodesService {
 
     @Autowired
     private UserCreationCodeRepository repository;
