254b63bb259ed9b3fc2ad77d9d270fd394702887
samuellsrobertson <samlsrobertson@gmail.com>
2025-01-09 14:13:18 +0000

    **Admin Controller** - Removed try-catch from requestNewUsers - Added exception handler to handle a bad request
    **Admin Controller Tests**
    - Added Tests (all passing)

--- src/main/java/com/mersey/rowing/club/crm/controller/AdminController.java
+++ src/main/java/com/mersey/rowing/club/crm/controller/AdminController.java
@@ -3,10 +3,9 @@ package com.mersey.rowing.club.crm.controller;
 import com.mersey.rowing.club.crm.controller.service.CreationCodesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -18,13 +17,15 @@ public class AdminController {
     @Autowired
     private CreationCodesService creationCodesService;
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return Map.of("error", e.getMessage());
+    }
+
     @GetMapping("/request_new_users")
     public ResponseEntity<?> requestNewUsers(@RequestParam(defaultValue = "1") int amount) {
-        try {
-            List<String> codes = creationCodesService.createNewUUID(amount);
-            return ResponseEntity.ok(Map.of("creation_codes", codes));
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
-        }
+        List<String> codes = creationCodesService.createNewUUID(amount);
+        return ResponseEntity.ok(Map.of("creation_codes", codes));
     }
 }
